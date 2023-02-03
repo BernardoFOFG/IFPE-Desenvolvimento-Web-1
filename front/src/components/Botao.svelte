d01016673535575cd70db8f015d483ff0d83ef4b
BernardoFOFG <bernardofofg@gmail.com>
2023-02-03 04:23:36 -0300

    add: botao component

---
+++ front/src/components/Botao.svelte
@@ -0,0 +1,6 @@
+<button
+  type="submit"
+  class="w-full bg-orange-900 py-3 px-4 text-white text-lg rounded font-semibold hover:ring-2 hover:ring-white transition-all"
+>
+  <slot name="button-child" />
+</button>
