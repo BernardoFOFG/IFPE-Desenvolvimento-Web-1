8f61a833d0ca9118ea88f2e7f6d893929f3ed9d8
BernardoFOFG <bernardofofg@gmail.com>
2023-02-03 04:24:28 -0300

    add: Header component

---
+++ front/src/components/Header.svelte
@@ -0,0 +1,12 @@
+<!-- Javascript -->
+<script>
+  import Cadastro from "../screens/Cadastro.svelte";
+
+</script>
+
+
+
+<!-- HTML -->
+<div class="flex flex-col">
+<h1 class="text-white text-4xl font-bold mb-2">Bem vindo ao LaranReceitas</h1>
+</div>
